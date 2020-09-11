26fc9a0a153991b2f09cb53b9b97004b2fb63302
HAOHANZHOU <haohanz@student.unimelb.edu.au>
2020-09-12 04:23:23 +1000

    update authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -6,7 +6,7 @@ const Author = mongoose.model("author");
 
 // function to handle a request to get all authors
 const getAllAuthors = async (req, res) => {
-    
+  res.send("Working on this feature");
   try {
     const all_authors = await Author.find();
     return res.send(all_authors);
